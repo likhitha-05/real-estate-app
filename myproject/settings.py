b8f9677bc6973603d0026ca7c1ac14d09f6953c5
likhitha-05 <banothulikhitha@gmail.com>
2022-07-31 10:35:34 +0530

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitnoise.middleware.WhitnoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
